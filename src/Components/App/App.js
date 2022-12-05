3452e6d9102087f11d23f2b0641b7ae6059cc67d
Callasaurus <c.sullivandrage@outlook.com>
2022-12-05 19:26:23 +0000

    refactored

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import {Routes, Route, useNavigate} from 'react-router-dom'
+import { Routes, Route, useNavigate } from 'react-router-dom'
 import Count from '../Count/Count';
 import DadJoke from '../DadJoke/DadJoke';
 
 function App() {
   const navigate = useNavigate()
-  
+
   const navigateCount = () => {
     navigate('/count', { replace: true })
   }
@@ -16,19 +16,15 @@ function App() {
 
   return (
     <div className="App">
-
       <h1><b>The most original app ever created</b></h1>
-      
-      <div className='buttons-parent'> 
+      <div className='buttons-parent'>
         <button className="home-button" onClick={navigateCount}> Count away</button>
         <button className="home-button" onClick={navigateDadJoke}> Want to laugh? </button>
       </div>
-
       <Routes>
-        <Route path="/count" element ={<Count/>} />
-        <Route path="/dadjoke" element ={<DadJoke/>} />
+        <Route path="/count" element={<Count />} />
+        <Route path="/dadjoke" element={<DadJoke />} />
       </Routes>
-
     </div>
   );
 }
--- src/Components/DadJoke/DadJoke.js
+++ src/Components/DadJoke/DadJoke.js
@@ -6,20 +6,13 @@ function DadJoke() {
     const [dadJoke, setDadJoke] = useState('')
 
     useEffect(() => {
-        async function getInitialJoke() {
-            const response = await fetch("https://icanhazdadjoke.com/", {
-                headers: { accept: "application/json" },
-              });
-            let data = await response.json()
-            setDadJoke(data.joke)
-        }
         getInitialJoke();
     }, []);
 
     async function getInitialJoke() {
         const response = await fetch("https://icanhazdadjoke.com/", {
             headers: { accept: "application/json" },
-          });
+        });
         let data = await response.json()
         setDadJoke(data.joke)
     }
@@ -27,9 +20,9 @@ function DadJoke() {
     return (
         <div className='dad-joke'>
             <div className='dad-joke-parent'>
-            <img id="dad-joke-img" alt="dadjoke" src="https://ichef.bbci.co.uk/news/624/mcs/media/images/81763000/jpg/_81763098_risitas.jpg" />
-            <button className='buttons' onClick={getInitialJoke}>Click for a new joke</button>
-            <h2>{dadJoke}</h2>
+                <img id="dad-joke-img" alt="dadjoke" src="https://ichef.bbci.co.uk/news/624/mcs/media/images/81763000/jpg/_81763098_risitas.jpg" />
+                <button className='buttons' onClick={getInitialJoke}>Click for a new joke</button>
+                <h2>{dadJoke}</h2>
             </div>
         </div>
     )
